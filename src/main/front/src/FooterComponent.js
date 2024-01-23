c027ea58bbc75d1ce8129fe72f612bcc3025c533
GyeongTae Kim <po04304a@gmail.com>
2024-01-23 23:45:54 +0100

    modified footer // added Links

--- src/main/front/src/FooterComponent.js
+++ src/main/front/src/FooterComponent.js
@@ -1,15 +1,20 @@
 import {Col, Container, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
 import React from "react";
+import {Link} from "react-router-dom";
 
 const FooterComponent = () => {
     return (
         <Navbar fixed="bottom" bg="black" className="h-25">
             <Container>
                 <Row className="text-white">
-                    <Col> Impressum </Col>
-                    <Col> AGB </Col>
-                    <Col> Datenschutz </Col>
-                    <Col> Contakt </Col>
+
+
+                    <Nav >
+                        <Nav.Link href="/Impressum" className="text-white">Impressum</Nav.Link>
+                        <Nav.Link href="/Datenschutz" className="text-white">Datenschutz</Nav.Link>
+                        <Nav.Link href="/AGB" className="text-white">AGB</Nav.Link>
+                        <Nav.Link href="/Kontakt" className="text-white">Kontakt</Nav.Link>
+                    </Nav>
                 </Row>
 
             </Container>
--- src/main/front/src/Main.js
+++ src/main/front/src/Main.js
@@ -2,32 +2,11 @@ import React, {useEffect, useRef} from 'react';
 import {Card, CardGroup, Col, Container, Row} from 'react-bootstrap';
 
 const Main = () => {
-    const div1Ref = useRef(null);
-    const div2Ref = useRef(null);
+
 
     useEffect(() => {
-        const handleScroll = () => {
-            const scrollTop = window.scrollY;
 
-            if (div1Ref.current && div2Ref.current) {
-                const div1Top = div1Ref.current.offsetTop;
-                const div2Top = div2Ref.current.offsetTop;
 
-                const fadeStart = div1Top;
-                const opacity = (scrollTop - fadeStart) / (div2Top - fadeStart);
-
-                if (opacity >= 0 && opacity <= 1) {
-                    div2Ref.current.style.opacity = opacity;
-                }
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        // Clean up event listener on component unmount
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
     }, []);
 
     return (
@@ -36,7 +15,7 @@ const Main = () => {
             <h2> Notice </h2>
             <CardGroup>
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160"/>
+
                     <Card.Body>
                         <Card.Title>Card title</Card.Title>
                         <Card.Text>
